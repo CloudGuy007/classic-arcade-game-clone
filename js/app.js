3f680d5cadfb5ef84a552ade03cc32a0e18f46bd
jamesparkes <parkesrjames@gmail.com>
2016-07-11 17:19:21 -0500

    add logic for player movement

--- js/app.js
+++ js/app.js
@@ -10,6 +10,52 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+var Player = function() {
+    /*
+     * COMMENT: add functionality to select 1 of 5 charaters
+     */
+    this.sprite = 'images/char-boy.png';
+
+    this.x = 2;
+    this.y = 5;
+};
+
+Player.prototype = {
+    update: function(dt) {
+
+    },
+    render: function() {
+        ctx.drawImage(
+            Resources.get(this.sprite),
+            this.convertPlayerX(this.x),
+            this.convertPlayerY(this.y)
+        );
+    },
+    handleInput: function(key) {
+        // change player's position based on direction
+        if(key === 'left' && this.x !== 0) {
+            this.x--;
+        } else if(key === 'right' && this.x !== 4) {
+            this.x++;
+        } else if(key === 'up' && this.y !== 0) {
+            this.y--;
+        } else if(key === 'down' && this.y !== 5) {
+            this.y++;
+        }
+
+        // render player after position change
+        this.render();
+    },
+    convertPlayerX: function(x) {
+        return (x * 101);
+    },
+    convertPlayerY: function(y) {
+        return (y * 83) - (83 / 2);
+    }
+}
+
+var player = new Player();
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
